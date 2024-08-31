1efe7ab879a98b4546e8139ebde957b9146678ff
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2024-08-31 09:49:26 +0200

    [Snyk] Security upgrade Npgsql from 3.2.7 to 4.0.14 (#6)
    * fix: src/DoomedDatabases.Postgres/DoomedDatabases.Postgres.csproj to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-DOTNET-NPGSQL-6825563
    
    * removed netstandard 1.3
    
    * test adaption
    
    * try running test with postgres db
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

--- test/DoomedDatabases.Tests/DatabaseFixture.cs
+++ test/DoomedDatabases.Tests/DatabaseFixture.cs
@@ -12,7 +12,7 @@ namespace DoomedDatabases.Tests
         public DatabaseFixture()
         {
             var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            TestDatabase = new TestDatabaseBuilder().WithConfiguration(configuration).WithTemplateDatabase("uuid_extension_enabled").Build();
+            TestDatabase = new TestDatabaseBuilder().WithConfiguration(configuration).Build();
             TestDatabase.Create();
 
             var builder = new DbContextOptionsBuilder<TestDbContext>();
